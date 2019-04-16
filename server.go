a127d4fa170c31f1d4df99af062394ff7a0049f5
lianyue <admin@lianyue.org>
2019-04-16 10:45:08 +0800

    fix ReadHeaderTimeout

--- server.go
+++ server.go
@@ -95,7 +95,7 @@ func (server *Server) Init() *Server {
 		server.IdleTimeout = time.Second * 300
 	}
 	if server.ShutdownTimeout == 0 {
-		server.ShutdownTimeout = server.WriteTimeout + server.ReadTimeout + server.ReadHeaderTimeouta
+		server.ShutdownTimeout = server.WriteTimeout + server.ReadTimeout + server.ReadHeaderTimeout
 	}
 
 	// gin
